0c5e5cb361174b539ea9e309628959c0b440ece4
Mingliang Chen <illuspas@gmail.com>
2015-02-11 15:10:45 +0800

    supports all APP_ABI

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,52 +2,54 @@
 LOCAL_PATH := $(call my-dir)
 
 common_SRC_FILES := \
-	source/compare.cc                   \
-	source/compare_common.cc            \
-	source/convert.cc                   \
-	source/convert_argb.cc              \
-	source/convert_from.cc              \
-	source/convert_from_argb.cc         \
-	source/convert_jpeg.cc              \
-	source/convert_to_argb.cc           \
-	source/convert_to_i420.cc           \
-	source/cpu_id.cc                    \
-	source/format_conversion.cc         \
-	source/mjpeg_decoder.cc             \
-	source/mjpeg_validate.cc            \
-	source/planar_functions.cc          \
-	source/rotate.cc                    \
-	source/rotate_argb.cc               \
-	source/row_any.cc                   \
-	source/row_common.cc                \
-	source/scale.cc                     \
-	source/scale_argb.cc                \
-	source/scale_common.cc              \
+	source/compare.cc                           \
+	source/compare_common.cc                    \
+	source/compare_neon.cc                      \
+	source/compare_neon64.cc                    \
+	source/compare_posix.cc                     \
+	source/compare_win.cc                       \
+	source/convert.cc                           \
+	source/convert_argb.cc                      \
+	source/convert_from.cc                      \
+	source/convert_from_argb.cc                 \
+	source/convert_jpeg.cc                      \
+	source/convert_to_argb.cc                   \
+	source/convert_to_i420.cc                   \
+	source/cpu_id.cc                            \
+	source/format_conversion.cc                 \
+	source/mjpeg_decoder.cc                     \
+	source/mjpeg_validate.cc                    \
+	source/planar_functions.cc                  \
+	source/rotate.cc                            \
+	source/rotate_argb.cc                       \
+	source/rotate_mips.cc                       \
+	source/rotate_neon.cc                       \
+	source/rotate_neon64.cc                     \
+	source/row_any.cc                           \
+	source/row_common.cc                        \
+	source/row_mips.cc                          \
+	source/row_neon.cc                          \
+	source/row_neon64.cc                        \
+	source/row_posix.cc                         \
+	source/row_win.cc                           \
+	source/scale.cc                             \
+	source/scale_argb.cc                        \
+	source/scale_common.cc                      \
+	source/scale_mips.cc                        \
+	source/scale_neon.cc                        \
+	source/scale_neon64.cc                      \
+	source/scale_posix.cc                       \
+	source/scale_win.cc                         \
 	source/video_common.cc
-
 
 common_CFLAGS := -fexceptions
 
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-#    common_CFLAGS += -DLIBYUV_NEON -D__ARM_NEON__ -march=armv7-a -mfpu=neon -mfloat-abi=softfp -mvectorize-with-neon-quad
-#    common_LDFLAGS += -Wl,--fix-cortex-a8
     LOCAL_ARM_NEON  := true
-    common_SRC_FILES += \
-    source/compare_neon.cc \
-    source/rotate_neon.cc \
-    source/row_neon.cc \
-    source/scale_neon.cc
 endif
 
 ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
-#    common_CFLAGS += -DLIBYUV_NEON -D__ARM_NEON__ -march=armv7-a -mfpu=neon -mfloat-abi=softfp -mvectorize-with-neon-quad
-#    common_LDFLAGS += -Wl,--fix-cortex-a8
     LOCAL_ARM_NEON  := true
-    common_SRC_FILES += \
-    source/compare_neon64.cc \
-    source/rotate_neon64.cc \
-    source/row_neon64.cc \
-    source/scale_neon64.cc
 endif
 
 
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,2 @@
-APP_ABI :=  armeabi armeabi-v7a arm64-v8a
+APP_ABI :=  all
 APP_STL := stlport_static
